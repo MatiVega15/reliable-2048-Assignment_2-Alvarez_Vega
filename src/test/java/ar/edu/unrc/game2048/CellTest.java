593777a09f930f4d487e4cd047af563c52790c70
MatiVega15 <matithomivega15@gmail.com>
2026-08-13 13:50:03 -0300

    Tests para el constructor de la clase Cell.

--- src/test/java/ar/edu/unrc/game2048/CellTest.java
+++ src/test/java/ar/edu/unrc/game2048/CellTest.java
@@ -1,8 +1,38 @@
 package ar.edu.unrc.game2048;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Testing de Unidad para los métodos públicos de la clase Cell.
  */
 public class CellTest {
 
+    /**
+     * Test para el constructor de la clase Cell con parámetro correcto.
+     */
+    @Test
+    public void constructorCorrectoTest () {
+        // Arrange.
+        int valor = 0;
+
+        // Act.
+        Cell celda = new Cell (valor);
+
+        // Assert.
+        assertTrue (celda.isEmpty ());
+        assertEquals (valor, celda.getValue ());
+    }
+
+    /**
+     * Test para el constructor de la clase Cell con parámetro incorrecto.
+     */
+    @Test
+    public void constructorExcepcionTest () {
+        // Arrange.
+        int valor = -1;
+
+        // Act - Assert.
+        assertThrows (IllegalArgumentException.class, () -> {Cell celula = new Cell (valor);});
+    }
 }
